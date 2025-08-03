b160f5573db60200bd4703360ac0ae54439e3f72
SnaetWarre <warre.snaet@student.howest.be>
2025-08-03 18:29:58 +0200

    fix eww bar

--- eww/scripts/workspace.sh
+++ eww/scripts/workspace.sh
@@ -12,25 +12,28 @@ ws() {
     for ((i=1; i<=workspaces; i++)); do
         windows=$(echo "$workspace_data" | jq -r "[.[] | select(.id == ${i})] | .[0]?.windows // 0")
         if [[ "$current_workspace" == "$i" ]]; then
-            workspace_icon+=(" ")
+            workspace_icon+=("●")
             workspace_class+=("visiting")
         elif [[ "$windows" -gt 0 ]]; then
-            workspace_icon+=(" ")
+            workspace_icon+=("○")
             workspace_class+=("occupied")
         else
-            workspace_icon+=(" ")
+            workspace_icon+=("○")
             workspace_class+=("free")
         fi
     done
 
-    output="(box :class \"ws\" :halign \"end\" :orientation \"h\" :spacing 5 :space-evenly \"false\""
+    output="(box :class \"ws\" :halign \"end\" :orientation \"h\" :spacing 8 :space-evenly \"false\""
     for i in {1..5}; do
         idx=$((i-1))
         output+=" (eventbox :onclick \"hyprctl dispatch workspace $i\" :cursor \"pointer\" :class \"${workspace_class[$idx]}\" (label :text \"${workspace_icon[$idx]}\"))"
     done
     output+=")"
-    /usr/bin/eww update workspaces-output="$output"
+    echo "$output"
 }
+
+# Initial output
+ws
 
 HYPRLAND_SIGNATURE_ACTUAL=$(ls -td /run/user/1000/hypr/*/ | head -n1 | xargs basename)
 SOCKET="/run/user/1000/hypr/${HYPRLAND_SIGNATURE_ACTUAL}/.socket2.sock"
