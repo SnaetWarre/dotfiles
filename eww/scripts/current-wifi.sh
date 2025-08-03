80f648300be4ca5b41f6f2de5c5bbfeb79d3d222
SnaetWarre <warre.snaet@student.howest.be>
2025-08-03 10:56:33 +0200

    updating configuration, fuck that menu launcher lol

--- eww/scripts/current-wifi.sh
+++ eww/scripts/current-wifi.sh
@@ -3,12 +3,12 @@
 wifi_info=$(nmcli -t -f active,ssid,signal dev wifi | grep '^yes')
 
 if [[ -z "$wifi_info" ]]; then
-    echo '{"icon": "睊", "ssid": "Disconnected", "strength": 0}'
+    echo '{"icon": "󰤭", "ssid": "Disconnected", "strength": 0}'
     exit 0
 fi
 
 ssid=$(echo "$wifi_info" | cut -d: -f2)
 signal=$(echo "$wifi_info" | cut -d: -f3)
-icon="直"
+icon="󰤨 "
 
 echo "{\"icon\": \"$icon\", \"ssid\": \"${ssid^^}\", \"strength\": $signal}"
